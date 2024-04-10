7fc8894cb5518b11124f0ad19b25b3876e2fc1ee
Laurence Luo <admin@lzc.app>
2024-04-10 09:53:04 +0800

    NotMe console project

---
+++ NotMe_Console/NtRaiseHardError/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NtRaiseHardError {
+    internal abstract class Program {
+        [DllImport("ntdll.dll")]
+        private static extern uint RtlAdjustPrivilege(int privilege, bool bEnablePrivilege, bool isThreadPrivilege, out bool previousValue);
+
+        [DllImport("ntdll.dll")]
+        private static extern uint NtRaiseHardError(uint errorStatus, uint numberOfParameters, uint unicodeStringParameterMask, IntPtr parameters, uint validResponseOption, out uint response);
+
+        private const int Privilege = 19;
+
+        private const uint ErrorStatus = 0xc0000022;
+
+        private const uint ValidResponseOption = 6;
+
+        private static void Main() {
+            _ = RtlAdjustPrivilege(Privilege, true, false, out _);
+
+            NtRaiseHardError(ErrorStatus, 0, 0, IntPtr.Zero, ValidResponseOption, out _);
+        }
+    }
+}
---
+++ NotMe_Console/RtlSetProcessIsCritical/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace RtlSetProcessIsCritical {
+    internal abstract class Program {
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern void RtlSetProcessIsCritical(uint newValue, uint oldValue, uint checkFlag);
+
+        private static void Main() {
+            try {
+                Process.EnterDebugMode();
+            } catch (Exception) {
+                var proc = new Process {
+                    StartInfo = {
+                        FileName = Assembly.GetExecutingAssembly().Location, UseShellExecute = true, Verb = "runas"
+                    }
+                };
+                proc.Start();
+            }
+
+            RtlSetProcessIsCritical(1, 0, 0);
+
+            Environment.Exit(1);
+        }
+    }
+}
